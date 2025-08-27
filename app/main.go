b8aab477e0ebb90228fd51aba7561afc99b9a3ce
gautamshubham <ichbingautam@gmail.com>
2025-08-27 17:58:53 +0530

    added the infinite lister with go routine

--- app/main.go
+++ app/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
@@ -18,10 +20,26 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Println("Start to bind to port 6379")
-  conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		go handleConnection(conn)
 	}
-	conn.Write([]byte("+PONG\r\n"))
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		text := scanner.Text()
+		fmt.Println("Received command: " + text)
+		if strings.TrimSpace(text) == "PING" {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+		//conn.Write([]byte("-Error invalid command: '" + text + "'\r\n"))
+	}
 }
